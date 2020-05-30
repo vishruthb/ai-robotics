9f2d6d6528ea853e5a3964f25aa5dd0fb7f2174b
Vishruth Bharath <31827869+vishruthb@users.noreply.github.com>
2020-05-30 00:14:19 -0700

    Create benchmark.py

---
+++ benchmark.py
@@ -0,0 +1,40 @@
+set -u
+
+# # Debug trace
+# set -x
+
+# Number of times to run the unit tests
+N=10
+
+# Name of the build directory
+BUILD_DIR_NAME=build
+
+# Get the script directory
+PRG_PATH="$(readlink -f "$0")"
+PRG_DIR="$(dirname "$PRG_PATH")"
+UTEST_DIR="$PRG_DIR/../$BUILD_DIR_NAME"
+
+#############
+# Functions #
+#############
+
+# Get the real time in second (given the output of time command)
+get_real_time() {
+    grep "real" | cut -d' ' -f2
+}
+
+# Run all query unit tests
+run_utests() {
+    cd "$UTEST_DIR";
+    make -j4 test ARGS=-j4
+    cd -
+}
+
+########
+# Main #
+########
+
+for i in $(seq 1 $N); do
+    echo "Run unit test suite ($i/$N)" 1>&2
+    (time -p run_utests 1>/dev/null) 2>&1
+done | get_real_time | st | column -t
